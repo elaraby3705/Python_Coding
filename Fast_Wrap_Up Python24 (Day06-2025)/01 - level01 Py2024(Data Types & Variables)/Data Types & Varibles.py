27cc53273dac31284319e7250a48c89fc94190d0
Ibn-elaraby37 <h.ibrahim3705@gmail.com>
2025-06-11 21:02:59 +0300

    Py2024 quick  walk on data types and variables

---
+++ Fast_Wrap_Up Python24 (Day06-2025)/01 - level01 Py2024(Data Types & Variables)/Data Types & Varibles.py
@@ -0,0 +1,29 @@
+#
+x = "Hammad"
+print(x)
+print(type(x))
+y = 'Hammad'
+print(y)
+x = 10
+print(type(x))
+
+z= 10.5
+print(z)
+print(type(z))
+print(str(z))
+print(type(str(z)))
+
+hi = "Hello "
+wor = "world"
+print(f"{hi}\n{wor}")
+welcomeMessage= f"{hi} \n{wor}"
+print(type(welcomeMessage))
+
+od = True
+print(od)
+print(type(od))
+num = float(90.0 )
+print(type(num))
+
+num = 90.0202230
+print(type(num))
